fc457a5faadda063942f45d09cd7bd44559e087f
Shota999 <45195110+Shota999@users.noreply.github.com>
2022-11-10 12:04:08 +0400

    main

--- src/components/menu/Menu.js
+++ src/components/menu/Menu.js
@@ -8,14 +8,14 @@ function Nav({ title }) {
 
 const Menu = () => {
   const menuItems = [
-    { title: "ჩვენ შესახებ" },
-    { title: "სექტორი" },
-    { title: "პრატქიკა" },
-    { title: "კრიტიკული ხედვა" },
-    { title: "სიახლეები" },
-    { title: "ვაკანსია" },
-    { title: "კონტაქტი" },
-    { title: "ჩვენი კლიენტები" },
+    { title: "ჩვენ შესახებ", id: 1 },
+    { title: "სექტორი", id: 2 },
+    { title: "პრატქიკა", id: 3 },
+    { title: "კრიტიკული ხედვა", id: 4 },
+    { title: "სიახლეები", id: 5 },
+    { title: "ვაკანსია", id: 6 },
+    { title: "კონტაქტი", id: 7 },
+    { title: "ჩვენი კლიენტები", id: 8 },
   ];
   return (
     <ul className="menu">
